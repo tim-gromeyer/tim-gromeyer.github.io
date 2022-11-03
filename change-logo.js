3fdcbb60451787a5dfdcfaa405d6fa8ae30ee765
software-made-easy <software-made-easy@users.noreply.github.com>
2022-11-03 20:41:03 +0000

    deploy: 818c76ddb6f5e54bc5ccc7bce57e39ad70044981

--- change-logo.js
+++ change-logo.js
@@ -1,16 +1,22 @@
 function switchLogo() {
     if(document.title == "MarkdownEdit") {
+        // Change logo
         document.all.projectlogo.children[0].src="MarkdownEdit.svg"
+        // Change project name
         document.all.projectname.textContent="MarkdownEdit"
+        // Change project brief
         document.all.projectbrief.textContent="Simple program for editing Markdown files"
+        // Update github corner link
+        document.getElementsByClassName("github-corner")[0].href="https://github.com/software-made-easy/MarkdownEdit"
     } else if (document.title == "Converter") {
         document.all.projectlogo.children[0].src="Converter.svg"
         document.all.projectname.textContent="Converter"
         document.all.projectbrief.textContent="Simple program for converting strings"
+        document.getElementsByClassName("github-corner")[0].href="https://github.com/software-made-easy/Converter"
     } else {
-        document.all.projectlogo.children[0].width=0
-        document.all.projectname.textContent="software-made-easy"
-        document.all.projectbrief.textContent="Hobby programmer from germany"
+        // Hide logo
+        var logo = document.getElementById("projectlogo")
+        logo.parentNode.removeChild(logo)
     }
 }
 
