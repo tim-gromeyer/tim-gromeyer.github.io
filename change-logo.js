b2e7cb7c3cf4e05604ed7e9e49b2bf57ef339877
tim-gromeyer <tim-gromeyer@users.noreply.github.com>
2022-11-12 22:27:24 +0000

    deploy: 2b4a036863fb2bf9d12dfc190c5e3e8a09503739

--- change-logo.js
+++ change-logo.js
@@ -1,7 +1,24 @@
+function changeFavicon(src) {
+    var link = document.createElement('link'),
+
+    oldLink = document.getElementById('dynamic-favicon');
+
+    link.id = 'dynamic-favicon';
+    link.rel = 'icon';
+    link.href = src;
+    if (oldLink) {
+        document.head.removeChild(oldLink);
+    }
+
+    document.head.appendChild(link);
+}
+
 function switchLogo() {
     if(document.title == "MarkdownEdit") {
         // Change logo
         document.all.projectlogo.children[0].src="MarkdownEdit.svg"
+        // Change favicon
+        changeFavicon("MarkdownEdit.svg")
         // Change project name
         document.all.projectname.textContent="MarkdownEdit"
         // Change project brief
@@ -10,6 +27,7 @@ function switchLogo() {
         document.getElementsByClassName("github-corner")[0].href="https://github.com/tim-gromeyer/MarkdownEdit"
     } else if (document.title == "Converter") {
         document.all.projectlogo.children[0].src="Converter.svg"
+        changeFavicon("Converter.svg")
         document.all.projectname.textContent="Converter"
         document.all.projectbrief.textContent="Simple program for converting strings"
         document.getElementsByClassName("github-corner")[0].href="https://github.com/tim-gromeyer/Converter"
